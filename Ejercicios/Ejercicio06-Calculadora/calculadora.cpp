e005160eaee04058f390f3dc92ef2373bed14bc0
Angela Castro <accastros@unah.hn>
2020-07-12 23:43:50 -0600

    agregando TAREA1

---
+++ Ejercicios/Ejercicio06-Calculadora/calculadora.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+
+    cout<< "Ingrese el primer numero:";
+    cin>> a;
+    cout<< "Ingrese el segundo numero:";
+    cin>> b;
+    cout<< endl;
+
+    cout<< "La suma de a + b = "<< a+b <<endl;
+    
+    cout<< "La resta de a - b = "<< a-b <<endl;
+    
+    cout<< "La multiplicacion de a * b = "<< a*b <<endl;
+    
+    cout<< "La division de a / b = "<< a/b <<endl;
+
+    return 0;
+}
