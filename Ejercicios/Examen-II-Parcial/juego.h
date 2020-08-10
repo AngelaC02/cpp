c64131ad907f3e066851e0793b9e4ef8618e3709
Angela Castro <accastros@unah.hn>
2020-08-09 18:37:22 -0600

    Examen hecho!

---
+++ Ejercicios/Examen-II-Parcial/juego.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void inicializarArreglo();
+void dificultad();
+void gotoxy(int x,int y);
+void pintar();
+bool gameover();
+void puntosx();
+void proceso(char &tecla, int &puntos);
